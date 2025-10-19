a23aaa9dc01e6bbccabdb03d6e45f3c8fd7f8f76
AveCezarus <150462525+AveCezarus@users.noreply.github.com>
2025-10-19 17:57:37 +0300

    ProjectTree

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+import LoginView from '@/views/LoginView.vue'
+import RegisterView from '@/views/RegisterView.vue'
+import CarDetailsView from '@/views/CarDetailsView.vue'
+
+const routes = [
+  { path: '/', name: 'home', component: HomeView },
+  { path: '/login', name: 'login', component: LoginView },
+  { path: '/register', name: 'register', component: RegisterView },
+  { path: '/cars/:id', name: 'car-details', component: CarDetailsView },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
